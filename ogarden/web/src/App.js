fe80ad3aae6340d94c795c923dcbad7acfe5d84c
Abdelrahman Abomosa <126721827+ABOOAB@users.noreply.github.com>
2024-01-31 02:06:04 +0300

    add

---
+++ ogarden/web/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+
+import Navigation from './components/Navigation.js';
+import ChartCard from './components/ChartCard.js';
+import WeatherCard from './components/WeatherCard.js';
+
+
+export default function App() {
+  const [log, setLog] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/log').then(res => res.json()).then(data => {
+      setLog(Object.values(data));
+    });
+  }, []);
+  
+
+  return (
+    <div>
+      <Navigation />
+        <Container>
+          <ChartCard log={log} />
+          <hr />
+          <h2>History</h2>
+          {log.slice(0).reverse().map(l => <WeatherCard data={l} />)}
+        </Container>
+    </div>
+  );
+}
+
